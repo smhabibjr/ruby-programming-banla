cf7ba03534d16c209410a7c240f829472cd50ff4
HabibJR <smhabib.abir2030@gmail.com>
2022-08-21 19:02:16 +0200

    add puts, print and p

--- index.rb
+++ index.rb
@@ -1,1 +1,11 @@
-print "hello world"
+#print something it will be one the same line as your last message.
+print "hello world \n"
+print "hi there"
+
+#puts something it will print every message its own line.
+puts "hello world"
+puts "hi there"
+
+#p is a method that shows a more “raw” version of an object.
+p "hello world"
+p 250
