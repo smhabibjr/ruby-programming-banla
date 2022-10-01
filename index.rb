207c7df55d69b21463cd63810be297a19ef6e841
HabibJR <smhabib.abir2030@gmail.com>
2022-10-01 23:10:39 +0200

    update

--- index.rb
+++ index.rb
@@ -1,8 +1,58 @@
-# basic Job interview questions and solutions
+# Useful Ruby String Methods
 
-num_01 = '50'.to_i # string to integer
-num_02 = 5.to_s # integer to string
+# 1. Get the length of a String #length method
 
-num_03 = '50'.to_f # String to float data type
+#string  = "hi there j"
+#puts string.size
 
-puts num_03
+
+# 2. Reverse a String #reverse method
+
+#puts "hi there".reverse
+
+
+# 3. Search for one or more characters of a String # include method
+
+#string = "this is habib"
+#puts string.include?("Habib")
+
+
+# 4. Replace characters in a String  # sub method
+
+#string = "My name is Habib. Habib live is germany"
+
+# puts  string.gsub("Habib","Karim")
+
+# 5. Split a String # split mehtod
+
+#p string.split
+
+
+# 6. Trim a String # strip method
+
+string = "hi there \n"
+p string
+
+
+
+
+
+
+
+# 7. add a String before another String
+
+
+# 8. Insert a String
+
+
+# 9. Methods for changing the case of a String
+
+
+
+# 10 Add Strings
+
+
+#11 Get substring
+
+
+# 12 Empty String check
