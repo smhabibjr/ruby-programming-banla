5a4c6517dd6a9a498dfa3886efe0ceb52bbc6410
HabibJR <smhabib.abir2030@gmail.com>
2022-10-01 23:31:10 +0200

    part 2

--- index.rb
+++ index.rb
@@ -1,44 +1,5 @@
 # Useful Ruby String Methods
 
-# 1. Get the length of a String #length method
-
-string  = "hi there"
-puts string.size
-
-
-# 2. Reverse a String #reverse method
-
-puts "hi there".reverse
-
-
-# 3. Search for one or more characters of a String # include method
-
-string = "this is habib"
-puts string.include?("Habib")
-
-
-# 4. Replace characters in a String  # sub method
-
-string = "My name is Habib. Habib live is germany"
-
-puts  string.gsub("Habib","Karim")
-
-# 5. Split a String # split mehtod
-
-p string.split
-
-
-# 6. Trim a String # strip method
-
-string = "hi there \n"
-p string
-
-
-
-
-
-
-
 # 7. add a String before another String
 
 
