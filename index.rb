afa5bde5aba3c749ced2fe370d149de4fb263f36
HabibJR <smhabib.abir2030@gmail.com>
2022-10-03 23:32:57 +0200

    code challenge one

--- index.rb
+++ index.rb
@@ -1,38 +1,32 @@
 # Useful Ruby String Methods
 
 # 7. add a String before another String # prepend("hello ")
+sentence = "world"
+puts sentence.prepend("Hello ")
 
-#sentence = "world"
-#puts sentence.prepend("Hello ")
 # 8. Insert a String # insert(position, new string)
 
-#puts sentence.insert(sentence.length, " Hello "
+puts sentence.insert(sentence.length, " Hello "
 
 # 9. Methods for changing the case of a String
 
-#sentence = "hi there."
-#puts sentence.capitalize
-
-
+sentence = "hi there."
+puts sentence.capitalize
 
 # 10 Add Strings # concat() methods
 
-#sent1 = "This is"
-#sent2 = "Habib"
+sent1 = "This is"
+sent2 = "Habib"
 
-#puts sent1.concat(" ").concat(sent2)
+puts sent1.concat(" ").concat(sent2)
 
 
 
 #11 Get substring # slice(0, 5)
 
-#sent = "This is Habib"
+sent = "This is Habib"
 
-#puts sent.slice(sent.length - 5, sent.length)
-
-
-
-
+puts sent.slice(sent.length - 5, sent.length)
 
 
 # 12 Empty String check # .empty?
